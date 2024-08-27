74ebb49d221189b104c43467593e20d1faad1d6c
Michael Teuscher <70554955+mfteuscher@users.noreply.github.com>
2024-08-26 23:27:31 -0600

    create a setup_vscode script for linking settings.json

---
+++ scripts/setup_vscode.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+# Symlink ~/.dotfiles/.config/vscode/settings.json to ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.dotfiles/.config/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
